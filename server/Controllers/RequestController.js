516c13e326be73181158ae462a2d8c51922d5137
nguyenthaingoc2002 <79910218+nguyenthaingoc2002@users.noreply.github.com>
2022-12-04 21:25:53 +0700

    commit

--- server/Controllers/RequestController.js
+++ server/Controllers/RequestController.js
@@ -10,17 +10,17 @@ module.exports.createRequest = async (req, res) => {
 
     const type = req.body.type;
     const requestA = await new RequestModel({
-        requester: requesterId,
-        recipient: recipientId,
-        status: 1,
-        type: type
-      }).save();
-      const requestB = await new RequestModel({
-        requester: requesterId,
-        recipient: recipientId,
-        status: 2,
-        type: type
-      }).save();
+      requester: requesterId,
+      recipient: recipientId,
+      status: 1,
+      type: type,
+    }).save();
+    const requestB = await new RequestModel({
+      requester: requesterId,
+      recipient: recipientId,
+      status: 2,
+      type: type,
+    }).save();
     requester.requestList.push(requestA._id);
     recipient.requestList.push(requestB._id);
     requester.save();
@@ -28,7 +28,39 @@ module.exports.createRequest = async (req, res) => {
     return res.status(200).json({
       success: true,
       msg: "successful",
-      data: ""
+      data: "",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      msg: error.message,
+    });
+  }
+};
+
+module.exports.getRequest = async (req, res) => {
+  try {
+    const request = RequestModel.findById(req.params.id);
+    return res.status(200).json({
+      success: true,
+      msg: "successful",
+      data: request,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      msg: error.message,
+    });
+  }
+};
+
+module.exports.getAllRequest = async (req, res) => {
+  try {
+    const allRequest = RequestModel.find({requester: req.body.id});
+    return res.status(200).json({
+      success: true,
+      msg: "successful",
+      data: allRequest,
     });
   } catch (error) {
     return res.status(500).json({
