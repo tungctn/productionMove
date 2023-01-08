61a146dc04dc2d67a8bf82bb399a49dfd3fa206a
tungjav <tungchuyenthainguyen@gmail.com>
2023-01-09 03:05:11 +0700

    add cor

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:8001/api",
+  baseURL: "https://plm-api.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
     Accept: "application/json",
