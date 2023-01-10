8edd60dbdef6991131cd3c3081778b4ed0ab672c
tungctn <78474449+tungctn@users.noreply.github.com>
2023-01-11 02:52:48 +0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const corsOptions = {
   origin: [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    "https://plm-api.onrender.com",
+    "https://plm-deploy.onrender.com/",
   ],
   credentials: true,
   exposedHeaders: ["set-cookie"],
