b88a7f9cc1f6d01b048727c3fe64f36952d51d23
tungjav <tungchuyenthainguyen@gmail.com>
2023-03-23 23:25:05 +0700

    abc

--- client/testing/spec.js
+++ client/testing/spec.js
@@ -1,18 +1,20 @@
-describe('Protractor Demo App', function () {
-  it('should have a title', function () {
-    browser.waitForAngularEnabled(false);
-    browser.get(process.env.APP_URL);
-    const email = element(by.id('basic_email'));
-    email.sendKeys('store1@gmail.com');
-    const password = element(by.id('basic_password'));
-    password.sendKeys('123456');
-    const button = element(by.css('button.ant-btn'));
-    button.getText().then((text) => {
-      if (text === 'Đăng nhập') {
-        button.click();
-      }
-    });
+// describe('Protractor Demo App', function () {
+//   it('should have a title', function () {
+//     browser.waitForAngularEnabled(false);
+//     browser.get('http://localhost:5001');
+//     const email = element(by.id('basic_email'));
+//     email.sendKeys('store1@gmail.com');
+//     const password = element(by.id('basic_password'));
+//     password.sendKeys('123456');
+//     const button = element(by.css('button.ant-btn'));
+//     button.getText().then((text) => {
+//       if (text === 'Đăng nhập') {
+//         button.click();
+//       }
+//     });
 
-    browser.sleep(5000);
-  });
-});
+//     browser.sleep(5000);
+//   });
+// });
+
+console.log(process.env.APP_URL);
